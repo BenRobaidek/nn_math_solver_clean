5516a5d93024f57fc76e3f1d5cc8b0f069425057
Ben Robaidek <brobaidek@gmail.com>
2018-01-02 16:33:26 -0800

    pre class acc first commit

--- classifier/test.py
+++ classifier/test.py
@@ -13,30 +13,53 @@ from evalTest import eval,test
 from torchtext.vocab import GloVe
 
 def main():
-    cuda = int(torch.cuda.is_available())-1
+    args = parseArgs()
+
+    cuda = int(torch.cuda.is_available())
 
     TEXT = data.Field(lower=True,init_token="<start>",eos_token="<end>")
     LABELS = data.Field(sequential=False)
 
-    train, val, test = data.TabularDataset.splits(
-        path='../tencent/data/', train='train_0.8.tsv',
-        validation='train_val_0.8_common.tsv', test='train_val_0.8_uncommon.tsv', format='tsv',
+    train, truth, preds = data.TabularDataset.splits(
+        path='', train=args.train, validation=args.truth, test=args.preds, format='tsv',
         fields=[('text', TEXT), ('label', LABELS)])
 
     TEXT.build_vocab(train)
     LABELS.build_vocab(train)
 
-    train_iter, val_iter, test_iter = data.BucketIterator.splits(
+    train_iter, truth_iter, preds_iter = data.BucketIterator.splits(
         (train, val, test), batch_sizes=(8, 8, 8),
         sort_key=lambda x: len(x.text), repeat=False)
 
-    #path = raw_input("enter model path: ")
-    model = torch.load('../tencent/models/common0.8/best_model.pt')
+    model = torch.load(args.model)
 
-    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(val_iter, model, TEXT, 300, LABELS)
+    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(truth_iter, model, TEXT, 300, LABELS)
     print('COMMON ACCURACY:', accuracy)
-    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(test_iter, model, TEXT, 300, LABELS)
+    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(preds_iter, model, TEXT, 300, LABELS)
     print('UNCOMMON ACCURACY:', accuracy)
+
+    perClassAcc(args.truth, args.preds)
+
+def perClassAcc(truth_path, preds_path):
+    """
+    Compute/print per class accuracy
+    """
+    truth = open(truth_path).readlines()
+    preds = open(preds_path).readlines()
+    print(truth)
+    print(preds)
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='test')
+
+    parser.add_argument('-model', type=str, default='', help='path to model, [default: '']')
+    parser.add_argument('-train', type=str, default='', help='path to train data file, [default: '']')
+    parser.add_argument('-truth', type=str, default='', help='path to ground true file, usually validation file [default: '']')
+    parser.add_argument('-preds', type=str, default='', help='path to preds file [default: '']')
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
     main()
