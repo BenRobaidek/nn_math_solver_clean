b78230df7a4ccfef299240ee3726b97331dd3d1d
Ben Robaidek <brobaidek@gmail.com>
2018-01-02 16:52:27 -0800

    move preClassAcc to utils

--- classifier/test.py
+++ classifier/test.py
@@ -6,59 +6,38 @@ import torch.nn.functional as F
 from numpy import genfromtxt
 from torch.autograd import Variable
 
+import data
 import model as m
 from torchtext import data, datasets
+import mydatasets
 from evalTest import eval,test
 from torchtext.vocab import GloVe
+from vecHandler import Vecs
 
 def main():
-    args = parseArgs()
-
-    cuda = int(torch.cuda.is_available())
+    cuda = int(torch.cuda.is_available())-1
 
     TEXT = data.Field(lower=True,init_token="<start>",eos_token="<end>")
     LABELS = data.Field(sequential=False)
 
-    train, truth, preds = data.TabularDataset.splits(
-        path='', train=args.train, validation=args.truth, test=args.preds, format='tsv',
+    train, val, test = data.TabularDataset.splits(
+        path='../tencent/data/', train='train_0.8.tsv',
+        validation='train_dev_0.8_common.tsv', test='train_dev_0.8_uncommon.tsv', format='tsv',
         fields=[('text', TEXT), ('label', LABELS)])
 
     TEXT.build_vocab(train)
     LABELS.build_vocab(train)
 
-    train_iter, truth_iter, preds_iter = data.BucketIterator.splits(
+    train_iter, val_iter, test_iter = data.BucketIterator.splits(
         (train, val, test), batch_sizes=(8, 8, 8),
         sort_key=lambda x: len(x.text), repeat=False)
 
-    model = torch.load(args.model)
+    model = torch.load('../tencent/models/common_0.8/net-lstm_e25_bs8_opt-adam_ly1_hs300_dr2_ed200_fembFalse_ptembFalse_drp0.3/acc47.60_e6.pt')
 
-    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(truth_iter, model, TEXT, 300, LABELS)
+    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(val_iter, model, TEXT, 300)
     print('COMMON ACCURACY:', accuracy)
-    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(preds_iter, model, TEXT, 300, LABELS)
+    (avg_loss, accuracy, corrects, size, t5_acc, t5_corrects, mrr) = eval(test_iter, model, TEXT, 300)
     print('UNCOMMON ACCURACY:', accuracy)
-
-    perClassAcc(args.truth, args.preds)
-
-def perClassAcc(truth_path, preds_path):
-    """
-    Compute/print per class accuracy
-    """
-    truth = open(truth_path).readlines()
-    preds = open(preds_path).readlines()
-    print(truth)
-    print(preds)
-
-
-def parseArgs():
-    parser = argparse.ArgumentParser(description='test')
-
-    parser.add_argument('-model', type=str, default='../tencent/models/good_model.pt', help='path to model, [default: \'../tencent/models/good_model.pt\']')
-    parser.add_argument('-train', type=str, default='../tencent/data/', help='path to train data file, [default: \'\']')
-    parser.add_argument('-truth', type=str, default='', help='path to ground true file, usually validation file [default: '']')
-    parser.add_argument('-preds', type=str, default='', help='path to preds file [default: '']')
-
-    args = parser.parse_args()
-    return args
 
 if __name__ == '__main__':
     main()
---
+++ utils/perClassAcc.py
@@ -0,0 +1,21 @@
+def main(args):
+    """
+    Compute/print per class accuracy
+    """
+    truth = open(args.truth).readlines()
+    preds = open(args.preds).readlines()
+    print(truth)
+    print(preds)
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='test')
+
+    parser.add_argument('-truth', type=str, default='', help='path to ground true file, usually validation file [default: '']')
+    parser.add_argument('-preds', type=str, default='', help='path to preds file [default: '']')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    main()
