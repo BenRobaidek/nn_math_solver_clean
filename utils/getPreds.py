bd3558bb4f2c061ff203d6c921eef566ec4e9131
Ben Robaidek <brobaidek@gmail.com>
2018-01-04 18:20:57 -0800

    TabulagrDataset

--- utils/getPreds.py
+++ utils/getPreds.py
@@ -19,7 +19,7 @@ def main(args):
     TEXT = data.Field(lower=True,init_token="<start>",eos_token="<end>")
     LABELS = data.Field(sequential=False)
 
-    inp = data.TabulagrDataset(path=args.inp, format='tsv', fields=[('text', TEXT), ('label', LABELS)])
+    inp = data.TabularDataset(path=args.inp, format='tsv', fields=[('text', TEXT), ('label', LABELS)])
 
 
 def parseArgs():
