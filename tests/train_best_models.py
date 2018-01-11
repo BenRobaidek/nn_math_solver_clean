4449ce1c0c87bbc726248ce13b41c2dcfd6f7865
Ben Robaidek <benro@uw.edu>
2018-01-10 16:58:31 -0800

    train best models

---
+++ tests/train_best_models.py
@@ -0,0 +1,22 @@
+"""
+trains the best models for the classifier and seq2seq model
+"""
+from classifier import train
+
+def main():
+    print('Training classifier on full tencent data') 
+    
+    print('Training classifier on common0.2 tencent data')
+       
+    print('Training classifier on common0.4 tencent data')
+  
+    print('Training classifier on common0.6 tencent data')
+    
+    print('Training classifier on common0.6 tencent data')
+    
+    print('Training classifier on common0.8 tencent data')
+    
+    print('Training OpenNMT seq2seq on full tencent data')
+    
+if __name__ == '__main__':
+    main()
