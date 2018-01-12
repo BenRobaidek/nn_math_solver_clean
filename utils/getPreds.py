264d4eaedff26bf4e44c678928b871b7d4f89c4c
Ben Robaidek <brobaidek@gmail.com>
2018-01-12 11:11:19 -0800

    print inp data

--- utils/getPreds.py
+++ utils/getPreds.py
@@ -28,7 +28,7 @@ def main(args):
     #sort_key=lambda x: len(x.text)
     inp_iter = data.Iterator(inp, batch_size=8, repeat=False, train=False, sort=False, sort_within_batch=False)
     for example in inp_iter.data().examples:
-        print(example[0]
+        print(example[0])
 
     predictions = open(args.preds, 'w')
     for batch in inp_iter:
