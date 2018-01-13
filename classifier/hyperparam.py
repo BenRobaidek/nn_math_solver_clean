81e2e459fb3767755debd1aa71ffd8fe8069e0dc
Ben Robaidek <brobaidek@gmail.com>
2018-01-13 15:20:16 -0800

    clean up of train

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -11,19 +11,19 @@ rand = True
 
 data_path = '../tencent/data/working/basic/'
 
-mf = (1,2,3)
-net_type = ('lstm', 'gru')
-epochs = 100,
-bs = 8, 16, 64
-opt = ('adamax', 'adam', 'sgd')
-ly =  (1, 2, 4)
-hs = (100, 300, 500, 1000)
-num_dir = 2,
-embdim = (50, 100, 200, 300, 500, 1000,  1500)
+mf = (1,)
+net_type = ('lstm',)
+epochs = 3,
+bs = 8,
+opt = ('adamax',)
+ly =  1,
+hs = (100,)
+num_dir = 1,
+embdim = (50,)
 embfix = (False,)#True)
 ptemb = (False,)#True)
-dropout = (0, .3, .5, .7)
-pred_filter = (True, False)
+dropout = (0,)
+pred_filter = (True,)
 
 x = list(itertools.product(net_type, epochs, bs, opt, ly, hs, num_dir,
                             embdim, embfix, ptemb, dropout, mf, pred_filter))
@@ -41,7 +41,7 @@ for (net_type, epoch, bs, opt, ly, hs, num_dir, embdim, embfix, ptemb,
                     'pretrained_emb':ptemb, 'dropout':dropout,
                     'pred_filter':pred_filter}
         results = train(data_path=data_path, train_path='train.tsv',
-                val_path='val.tsv', test_path='test.tsv', mf=mf, epochs=5,
+                val_path='val.tsv', test_path='test.tsv', mf=mf, epochs=epoch,
                 bs=bs, opt=opt, net_type=net_type, ly=ly, hs=hs, num_dir=num_dir,
                 emb_dim=embdim, embfix=embfix, pretrained_emb=ptemb, dropout=dropout,
                 pred_filter=pred_filter, save_path='./', save=False, verbose=False)
