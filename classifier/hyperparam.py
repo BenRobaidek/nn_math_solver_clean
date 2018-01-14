3a2d17e51bead94eab37e800f8749fa498fa153a
Ben Robaidek <brobaidek@gmail.com>
2018-01-14 09:18:52 -0800

    working on hyperparam search

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -31,22 +31,23 @@ x = list(itertools.product(net_type, epochs, bs, opt, ly, hs, num_dir,
 if rand: random.shuffle(x)
 
 
-hyperparam_results = dict()
+hyperparam_results = []
 
 for (net_type, epoch, bs, opt, ly, hs, num_dir, embdim, embfix, ptemb,
                     dropout, mf, pred_filter) in x:
     if not (embfix and not ptemb):
-        json = dict()
-        json['hyperparams'] = {'mf':mf, 'epochs':epoch, 'bs':bs, 'opt':opt,
+        json_entry = dict()
+        json_entry['hyperparams'] = {'mf':mf, 'epochs':epoch, 'bs':bs, 'opt':opt,
                     'net_type':net_type, 'ly':ly, 'hs':hs, 'num_dir':num_dir,
                     'embdim':embdim, 'embfix':embfix,
                     'pretrained_emb':ptemb, 'dropout':dropout,
                     'pred_filter':pred_filter}
-        json['results'] = train(data_path=data_path, train_path='train.tsv',
+        json_entry['results'] = train(data_path=data_path, train_path='train.tsv',
                 val_path='val.tsv', test_path='test.tsv', mf=mf, epochs=epoch,
                 bs=bs, opt=opt, net_type=net_type, ly=ly, hs=hs, num_dir=num_dir,
                 emb_dim=embdim, embfix=embfix, pretrained_emb=ptemb, dropout=dropout,
                 pred_filter=pred_filter, save_path='./', save=False, verbose=False)
-        json['results'] = sorted(json['results'], key=lambda x: x['accuracy'])
-        #json['best'] =
-        print(hyperparam_results)
+        json_entry['results'] = sorted(json['results'], key=lambda x: x['accuracy'], reverse=True)
+        hyperparam_results = np.append(hyperparam_results, json_entry)
+
+print(hyperparam_results)
