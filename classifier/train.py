9e4e73e97772a8188072cb3cf2b6137304e0d8a9
Ben Robaidek <brobaidek@gmail.com>
2018-01-15 14:06:48 -0800

    volatile model inp

--- classifier/train.py
+++ classifier/train.py
@@ -97,6 +97,7 @@ def train(data_path, train_path, val_path, test_path, mf, epochs, bs, opt,
         for batch_count,batch in enumerate(train_iter):
             model.zero_grad()
             inp = batch.text.t()
+            inp.volatile = True
             print(inp)
 
             preds = model(inp)
