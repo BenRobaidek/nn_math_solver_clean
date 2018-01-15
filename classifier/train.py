040b3700628bfe83ebf8f1b9ffa6011455a4f3b0
Ben Robaidek <brobaidek@gmail.com>
2018-01-15 14:03:32 -0800

    volatile model inp

--- classifier/train.py
+++ classifier/train.py
@@ -63,7 +63,7 @@ def train(data_path, train_path, val_path, test_path, mf, epochs, bs, opt,
                     dropout=dropout,
                     net_type=net_type)
 
-    model.flatten_parameters()
+
     criterion = nn.CrossEntropyLoss()
 
     # Select optimizer
@@ -96,7 +96,7 @@ def train(data_path, train_path, val_path, test_path, mf, epochs, bs, opt,
         train_iter.repeat=False
         for batch_count,batch in enumerate(train_iter):
             model.zero_grad()
-            inp = batch.text.t()
+            inp = batch.text.t().volatile
 
             preds = model(inp)
             loss = criterion(preds, batch.label)
