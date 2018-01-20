6b59559009fff0720864aa976de4be49b776b9c8
Ben Robaidek <brobaidek@gmail.com>
2018-01-20 08:11:09 -0800

    buld the ans vocab

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -73,7 +73,7 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
         ans = ans.astype(float)
         #print('ans:', ans)
 
-        for pred_answer, pred_eq_id, answer, tgt_eq_id in zip(pred_answers, pred.data, ans, target.data):
+        for pred_answer, pred_eq_id, answer, tgt_eq_id in zip(pred_answers, preds.data, ans, target.data):
             try:
                 float(pred_answer)
                 if abs((pred_answer - answer) / answer) <= .02:
