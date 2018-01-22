ec7d755c0041535f3071c26e20c945aaa420147a
Ben Robaidek <brobaidek@gmail.com>
2018-01-22 13:22:58 -0800

    easier configs

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -34,8 +34,8 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
         corrects += preds.data.eq(target.data).sum()
 
         # True Acc
-        print('var_values:', var_values)
-        print('ans:', ans)
+        print('var_values:', var_values.data)
+        print('ans:', ans.data)
 
         # Rank 5
         _, t5_indices = torch.topk(logit, 5)
