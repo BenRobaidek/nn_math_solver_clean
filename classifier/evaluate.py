ccd223e556db72719684bd6d5324cc27a46880d8
Ben Robaidek <brobaidek@gmail.com>
2018-01-22 14:47:13 -0800

    print mroe things

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -48,6 +48,7 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
                 tgt = tgt.replace(k, var_value[k])
             prediction = prediction.strip('x=')
             tgt = tgt.strip('x=')
+            print('prediction:', prediction)
             print('tgt:', tgt)
             print('var_value:', var_value)
             print('answer:', answer)
