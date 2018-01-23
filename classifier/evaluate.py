45a8e6e4f179c3b072628e1ef8d4a5cf5870fc64
Ben Robaidek <brobaidek@gmail.com>
2018-01-23 11:21:26 -0800

    print it bad

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -64,7 +64,7 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
             tgt = tgt.strip('x =')
 
             # evaluate
-            if (not prediction.strip() == '80千米 / 小时') and (prediction.contains(r'\[s+\]')):
+            if (not prediction.strip() == '80千米 / 小时') and (not re.search(r'\[s+\]', prediction)):
                 prediction = eval(prediction)
             else:
                 prediction = None
