331dcbf7ba9a3b322242bf51bfff4ebf15fa3c50
Ben Robaidek <brobaidek@gmail.com>
2018-01-24 14:03:33 -0800

    answer correspond to eq

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -61,8 +61,9 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
             # replace ^ with ** in tgt equation
             tgt = tgt.replace('^', '**')
             print('tgt before:', tgt)
-            tgt = tgt.split(' / 100')
-            print('tgt mid:', tgt)
+            tgt = tgt.split()
+            for i in range(len(tgt)):
+                print(tgt[i])
             tgt = ' '.join(tgt)
             print('tgt after:', tgt)
             # remove = from equations
