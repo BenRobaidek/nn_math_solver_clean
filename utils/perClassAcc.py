c01b3342019abffea2f481cd31edc5e9da8ecb52
Ben Robaidek <brobaidek@gmail.com>
2018-01-25 11:13:00 -0800

    print more

--- utils/perClassAcc.py
+++ utils/perClassAcc.py
@@ -50,7 +50,7 @@ def main(args):
 
     all_results = sorted(all_results, key=lambda x: x[3], reverse=True)
     for line in all_results:
-        perClassAcc_file.write('' + line[0] + ' true_acc: ' + line[3] + ' ({}/{})'.format(line[1],float(line[1])+float(line[2]) + '\n'))
+        perClassAcc_file.write('' + line[0] + ' true_acc: ' + line[3] + ' ({}/{})'.format(line[1],float(line[1])+float(line[2])) + '\n')
     perClassAcc_file.close()
 
 def isFloat(f):
