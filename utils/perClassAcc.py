e7cba5147e1b90684a581c09fa1cfe74e41b4266
Ben Robaidek <brobaidek@gmail.com>
2018-01-25 10:31:58 -0800

    print more

--- utils/perClassAcc.py
+++ utils/perClassAcc.py
@@ -37,7 +37,7 @@ def main(args):
 
     all_results = []
     for eq, right, wrong in zip(itos, results[:,0], results[:,1]):
-        all_results = all_results.append([eq, right, wrong])
+        all_results = np.append(all_results, [eq, right, wrong], axis=0)
 
 def isFloat(f):
     try:
