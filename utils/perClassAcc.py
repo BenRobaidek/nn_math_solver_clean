e7bb2d3653d7fcdfc9d81bed9286033d80d6fb80
Ben Robaidek <brobaidek@gmail.com>
2018-01-25 09:47:11 -0800

    print more:

--- utils/perClassAcc.py
+++ utils/perClassAcc.py
@@ -18,9 +18,10 @@ def main(args):
             dictionary[eq] == [0,0]
         if isFloat(prediction): prediction = float(prediction)
         if isFloat(target): target = float(target)
+        print('eq:', eq)
         if isFloat(prediction) and isFloat(target) and abs(prediction - target) <= .002:
             print('dictionary.get(eq):', dictionary.get(eq))
-            print('dictionary.[eq]:', dictionary[eq])
+            print('dictionary[eq]:', dictionary[eq])
             dictionary[eq] = np.add(dictionary.get(eq), [1,0])
         else:
             #print('type(dictionary):', type(dictionary))
