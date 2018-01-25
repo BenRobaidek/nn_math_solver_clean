885b8bfb958a93a9f99ea06959043e623f36043d
Ben Robaidek <brobaidek@gmail.com>
2018-01-24 19:13:36 -0800

    print less

--- utils/perClassAcc.py
+++ utils/perClassAcc.py
@@ -19,13 +19,13 @@ def main(args):
         print('eq:', eq)
         print('type(eq):',type(eq))
         if isFloat(prediction) and isFloat(target) and abs(prediction - target) <= .002:
-            print('type(dictionary):', type(dictionary))
-            print('dictionary.get(eq) BEFORE',dictionary.get(eq))
-            print('np.add(dictionary.get(eq), [1,0])',np.add(dictionary.get(eq), [1,0]))
+            #print('type(dictionary):', type(dictionary))
+            #print('dictionary.get(eq) BEFORE',dictionary.get(eq))
+            #print('np.add(dictionary.get(eq), [1,0])',np.add(dictionary.get(eq), [1,0]))
             dictionary[eq] = np.add(dictionary.get(eq), [1,0])
-            print('dictionary.get(eq) AFTER',dictionary.get(eq))
+            #print('dictionary.get(eq) AFTER',dictionary.get(eq))
         else:
-            print('type(dictionary):', type(dictionary))
+            #print('type(dictionary):', type(dictionary))
             dictionary[eq] = np.add(dictionary.get(eq), [0,1])
     for k in dictionary.keys():
         print(k, dictionary.get(k))
