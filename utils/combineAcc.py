8f6e8eb2917dec4d6d79e67071081c5da80ff2f9
Ben Robaidek <brobaidek@gmail.com>
2018-01-26 14:02:00 -0800

    combined accuract

---
+++ utils/combineAcc.py
@@ -0,0 +1,24 @@
+import operator
+import argparse
+import numpy as np
+import torch
+
+def main(args):
+    """
+    prints combined classifier and seq2seq true accuracy
+    """
+    s2s_preds = open(args.s2s_preds, 'r')
+    classifier_preds = open(args.classifier_preds, 'r')
+    print(s2s_preds)
+    print(classifier_preds)
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='test')
+    parser.add_argument('-s2s_preds', type=str, default='/tmp/predictions.txt.correct', help='path to ground s2s predictions [default: '']')
+    parser.add_argument('-classifier_preds', type=str, default='./preds.txt', help='path to preds file [default: '']')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parseArgs()
+    main(args)
