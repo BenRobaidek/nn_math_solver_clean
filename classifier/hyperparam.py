147f03a6d0411a8e06bcc471d31e631e14dbadc6
Ben Robaidek <brobaidek@gmail.com>
2018-01-26 18:26:28 -0800

    diff configs

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -69,7 +69,7 @@ def main():
                                 ly=ly, hs=hs, num_dir=num_dir, emb_dim=embdim,
                                 embfix=bool(embfix), pretrained_emb=bool(ptemb),
                                 dropout=dropout, pred_filter=bool(pred_filter),
-                                save_path='./', save=False, verbose=False)
+                                save_path='./' + str(hyperparams) + '/', save=False, verbose=False)
                         results = sorted(results, key=lambda x: x['accuracy'],
                                 reverse=True)
                     except RuntimeError:
