d04222e796fcc096f10dc2f18304bdf5df8a57a1
Ben Robaidek <brobaidek@gmail.com>
2018-02-06 10:57:27 -0800

    eval sympy

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -68,7 +68,8 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
 
             print('prediction:', prediction)
             prediction = prediction.split(',')
-            
+            for k,p in enumerate(prediction):
+                prediction[k] = '0=(' + p.split('=')[1] + ') - (' + p.split('=')[0] + ')
             print('prediction:', prediction)
             sympy.solve(prediction.split(','), answer_variables)
 
