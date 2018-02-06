ef099786fffa7889d4b4360abd10609fc964d7dd
Ben Robaidek <brobaidek@gmail.com>
2018-02-06 11:07:39 -0800

    eval sympy

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -67,22 +67,23 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
             tgt = tgt.replace('^', '**')
             # remove = from equations
 
-            # get variables out of predicted equation
-            answer_variables = np.unique(re.findall(r'[a-z,A-Z]', prediction, flags=0))
-            print('answer_variables:', answer_variables)
-            #sympy.solve(prediction,answer_variables)
+            if (prediction is not '<unk>') and (tgt is not '<unk>'):
+                # get variables out of predicted equation
+                answer_variables = np.unique(re.findall(r'[a-z,A-Z]', prediction, flags=0))
+                print('answer_variables:', answer_variables)
+                #sympy.solve(prediction,answer_variables)
 
-            print('prediction:', prediction)
-            prediction = prediction.split(',')
-            for k,p in enumerate(prediction):
-                prediction[k] = '(' + p.split('=')[1] + ') - (' + p.split('=')[0] + ')'
-            prediction = ','.join(prediction)
-            print('prediction:', prediction)
+                print('prediction:', prediction)
+                prediction = prediction.split(',')
+                for k,p in enumerate(prediction):
+                    prediction[k] = '(' + p.split('=')[1] + ') - (' + p.split('=')[0] + ')'
+                prediction = ','.join(prediction)
+                print('prediction:', prediction)
 
-            try:
-                sympy.solve(prediction.split(','), answer_variables)
-            except ValueError as e:
-                print(e)
+                try:
+                    sympy.solve(prediction.split(','), answer_variables)
+                except ValueError as e:
+                    print(e)
 
             print('prediction:', prediction)
             print('tgt:', tgt)
