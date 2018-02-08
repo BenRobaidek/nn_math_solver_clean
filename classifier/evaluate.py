a9a475878ccdb80fcc20447027d309f6163f9806
Ben Robaidek <brobaidek@gmail.com>
2018-02-08 12:12:39 -0800

    print prediction

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -87,8 +87,9 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
                 if len(prediction) == 1: prediction = prediction[0]
                 answers = dict()
                 print(answer_variables)
-                print('prediction:', prediction)
+
                 if not len(np.unique(re.findall(r'\[[a-z]\]', ','.join(prediction)))) >= 1:
+                    print('prediction:', prediction)
                     expr = sympy.simplify(prediction)
                     symbols = sympy.symbols(str(answer_variables))
                     print('expr:', expr)
