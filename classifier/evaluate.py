6dcc495794087acb1950c7745edca4f2c26856ba
Ben Robaidek <brobaidek@gmail.com>
2018-02-08 18:00:42 -0800

    hyperparam configs

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -148,7 +148,7 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
     accuracy = 100.0 * corrects/size
     true_acc = 100.0 * true_corrects/size
     #print('acc:', accuracy)
-    print('true_acc:', true_acc)
+    #print('true_acc:', true_acc)
     #print('percent unk:', unk/size)
     #print('answer_correspond_to_equation:', answer_correspond_to_equation/size)
     #print()
