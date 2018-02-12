d3fb2774f373b95cd7060db54ba5dd4bd28fae02
Ben Robaidek <brobaidek@gmail.com>
2018-02-12 14:52:48 -0800

    print cross val true acc

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -84,8 +84,8 @@ def main():
                     hyperparam_results[str(hyperparams)] = cross_val_results
                     print('cross_val_results:', cross_val_results)
                     cross_val_true_acc = list(cross_val_results.values())
-                        for i,x in enumerate(cross_val_true_acc):
-                            print(x[0])
+                    for i,x in enumerate(cross_val_true_acc):
+                        print(x[0])
                     print('cross validation true acc:', cross_val_true_acc)
             with open(args.hyperparam_results, 'w') as f:
                 json.dump(hyperparam_results, f, indent=2)
