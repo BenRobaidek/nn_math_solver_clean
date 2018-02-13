346b8b9f45a7ae222bbd9ce7e8acb4775f3ac32f
Ben Robaidek <brobaidek@gmail.com>
2018-02-13 15:32:02 -0800

    return best class preds

--- classifier/train.py
+++ classifier/train.py
@@ -143,8 +143,8 @@ def train(data_path, train_path, val_path, test_path, mf, epochs, bs, opt,
 
         results = np.append(results, {'epoch':epoch, 'avg_loss':avg_loss,
             'accuracy':accuracy, 'true_acc':true_acc, 'corrects':corrects,
-            'size': size, 't5_acc':t5_acc, 't5_corrects':t5_corrects,
-                                                                    'mrr':mrr})
+            'size': size, 't5_acc':t5_acc, 't5_corrects':t5_corrects, 'mrr':mrr,
+            'preds': eval_preds})
         if verbose: print('\nEvaluation - loss: {:.6f}  acc: {:.4f}%({}/{}) ' \
                     'true_acc: {:.4f}%(todo/todo) t5_acc: {:.4f}%({}/{}) MRR:' \
                     '{:.6f}\n'.format(avg_loss, accuracy, corrects, size,
