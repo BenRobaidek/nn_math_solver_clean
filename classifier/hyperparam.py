0069ec5468610824167ab7ac1e902a8262d51deb
Ben Robaidek <brobaidek@gmail.com>
2018-02-13 10:38:27 -0800

    s22 cross val true acc

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -83,11 +83,11 @@ def main():
                         cross_val_results['cross_val_true_acc'] = cross_val_true_acc
 
                         # load s2s predictions
-                        s2s_predictions_k1 = open('../tencent/data/working/s2s/correctsk1.tsv').readlines()
-                        s2s_predictions_k2 = open('../tencent/data/working/s2s/correctsk2.tsv').readlines()
-                        s2s_predictions_k3 = open('../tencent/data/working/s2s/correctsk3.tsv').readlines()
-                        s2s_predictions_k4 = open('../tencent/data/working/s2s/correctsk4.tsv').readlines()
-                        s2s_predictions_k5 = open('../tencent/data/working/s2s/correctsk5.tsv').readlines()
+                        s2s_predictions_k1 = np.array(open('../tencent/data/working/s2s/correctsk1.tsv').readlines()).astype(bool)
+                        s2s_predictions_k2 = np.array(open('../tencent/data/working/s2s/correctsk2.tsv').readlines()).astype(bool)
+                        s2s_predictions_k3 = np.array(open('../tencent/data/working/s2s/correctsk3.tsv').readlines()).astype(bool)
+                        s2s_predictions_k4 = np.array(open('../tencent/data/working/s2s/correctsk4.tsv').readlines()).astype(bool)
+                        s2s_predictions_k5 = np.array(open('../tencent/data/working/s2s/correctsk5.tsv').readlines()).astype(bool)
                         print('s2s cross validation true accuracy:',
                                 np.average(np.sum(s2s_predictions_k1),
                                 np.sum(s2s_predictions_k2),
