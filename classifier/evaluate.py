620cdf4b5fdab62db91da9a78cfc5e34e57a38eb
Ben Robaidek <brobaidek@gmail.com>
2018-02-13 09:28:12 -0800

    streamlined solver

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -51,7 +51,7 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
         true_corrects += np.sum(pred_corrects)
 
         # solve tgt equations if possible, true iff solved correctly
-        equations = np.array(LABELS.vocab.itos)[np.array(target.data)]
+        equations = np.array(LABELS.vocab.itos)[np.array(batch.label.data)]
         tgt_corrects = solver.solve(equations, variables, answers)
         answer_correspond_to_equation += np.sum(tgt_corrects)
 
