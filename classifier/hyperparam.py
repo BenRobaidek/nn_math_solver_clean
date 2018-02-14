d75a44d154d18feabab44798c50d9c69d0bf94ee
Ben Robaidek <brobaidek@gmail.com>
2018-02-13 17:00:02 -0800

    return best class preds

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -169,8 +169,8 @@ def main():
                         print('Oops... Ran out of memory')
                     print('type(cross_val_results)', type(cross_val_results))
                     print('cross_val_results', cross_val_results)
-                    #for k in cross_val_results.keys():
-                    #    cross_val_results.get(k)[:].pop('preds')
+                    for k in cross_val_results.keys():
+                        cross_val_results.get(k)[:].pop('preds')
                     hyperparam_results[str(hyperparams)] = cross_val_results
 
             with open(args.hyperparam_results, 'w') as f:
