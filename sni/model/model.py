0779a9df08cb659119ffa39e82c78a610d2732e9
Ben Robaidek <brobaidek@gmail.com>
2018-02-16 11:29:29 -0800

    print useful things

--- sni/model/model.py
+++ sni/model/model.py
@@ -47,13 +47,15 @@ class Model(nn.Module):
         e = self.emb(inp)
 
         #e = inp
+        print('here1')
         if self.net_type == 'lstm':
             #self.lstm.flatten_parameters()
             _, (y,_) = self.lstm(e, hc)
-            print('here1')
+
         elif self.net_type == 'gru':
             #self.gru.flatten_parameters()
             _, y = self.gru(e, hc[0])
+            print('here0')
         print('here2')
         if self.num_dir==2:
             y = torch.cat([y[0:y.size(0):2], y[1:y.size(0):2]], 2)
