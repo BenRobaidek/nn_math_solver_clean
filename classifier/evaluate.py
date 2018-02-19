45001ec3e791bc4831f4425292844c810b3cca68
Ben Robaidek <brobaidek@gmail.com>
2018-02-19 13:21:57 -0800

    configs

--- classifier/evaluate.py
+++ classifier/evaluate.py
@@ -57,6 +57,7 @@ def evaluate(data_iter, model, TEXT, emb_dim, LABELS, VAR_VALUES, ANS, snis, pre
         # solve tgt equations if possible, true iff solved correctly
         equations = np.array(LABELS.vocab.itos)[np.array(batch.label.data)]
         tgt_corrects = solver.solve(equations, variables, answers)
+        print('pred_corrects:', pred_corrects)
         print('tgt_corrects:', tgt_corrects)
         answer_correspond_to_equation += np.sum(tgt_corrects)
 
