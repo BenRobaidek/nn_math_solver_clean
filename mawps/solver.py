6aa83e07bcc63f666e380c73751187d8bd156210
Ben Robaidek <brobaidek@gmail.com>
2018-02-21 14:08:23 -0800

    mawps sovler

--- mawps/solver.py
+++ mawps/solver.py
@@ -9,6 +9,7 @@ def solve(equations, variables, answers):
     test_dicts = open('../mawps/data/test.dicts').readlines()
     test = [x.split('\t')[1] for x in open('../mawps/data/test.tsv').readlines()]
     for pred_eq, gold_eq, var in zip(equations, test, test_dicts):
+        var = eval(var)
         print('pred_eq', pred_eq)
         print('gold_eq', gold_eq)
         print('var', var)
