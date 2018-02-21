dc7af92fa8b863f471669e7f72d3648faf5ac5f1
Ben Robaidek <brobaidek@gmail.com>
2018-02-21 12:53:46 -0800

    print config

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -197,7 +197,7 @@ def getThresh(class_predictions, s2s_predictions):
     """
     combines classifier and s2s results
     """
-    #assert len(class_predictions) == len(s2s_predictions)
+    assert len(class_predictions) == len(s2s_predictions)
     results = dict()
     for thresh in np.multiply(list(range(0,100)), .01):
         results[thresh] = np.sum([c[0] if c[1] > thresh else s for c,s in zip(class_predictions,s2s_predictions)])
@@ -205,7 +205,7 @@ def getThresh(class_predictions, s2s_predictions):
     return max(results, key=results.get)
 
 def combineCS(class_predictions, s2s_predictions, thresh):
-    #assert len(class_predictions) == len(s2s_predictions)
+    assert len(class_predictions) == len(s2s_predictions)
     return [c[0] if c[1] > thresh else s for c,s in zip(class_predictions,s2s_predictions)]
 
 if __name__ == '__main__':
