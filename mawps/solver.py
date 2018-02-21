ea6fcb97910ed98bcfd8a104bd47055af948a001
Ben Robaidek <brobaidek@gmail.com>
2018-02-21 14:06:47 -0800

    mawps sovler

--- mawps/solver.py
+++ mawps/solver.py
@@ -6,7 +6,14 @@ import re
 
 def solve(equations, variables, answers):
     corrects = np.array([])
-    test_dicts = open('../mawps/data/test.dicts')
-    print('test_dicts:', test_dicts)
+    test_dicts = open('../mawps/data/test.dicts').readlines()
+    test = [x.split('\t')[1] for x in open('../mawps/data/test.tsv').readlines()]
+    for pred_eq, gold_eq, var in zip(equations, test, test_dicts):
+        print('pred_eq', pred_eq)
+        print('gold_eq', gold_eq)
+        print('var', var)
+
+
+    #print('test_dicts:', test_dicts)
     corrects = np.ones(len(equations))
     return corrects.astype(bool)
