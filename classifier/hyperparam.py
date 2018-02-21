9613452903392c6eec4848962283534bffacd352
Ben Robaidek <brobaidek@gmail.com>
2018-02-21 12:55:25 -0800

    print config

--- classifier/hyperparam.py
+++ classifier/hyperparam.py
@@ -101,8 +101,8 @@ def main():
                         # seq2seq
                         ########################################################
                         # load s2s predictions
-                        s2s_validation_predictions_path = config['data_path'].strip('working/basic/') + 'output/s2s/corrects_valk1234.tsv'
-                        s2s_test_predictions_path = config['data_path'].strip('working/basic/') + 'output/s2s/corrects_testk5.tsv'
+                        s2s_validation_predictions_path = config['data_path'].strip('/working/basic/') + '/output/s2s/corrects_valk1234.tsv'
+                        s2s_test_predictions_path = config['data_path'].strip('/working/basic/') + '/output/s2s/corrects_testk5.tsv'
                         s2s_validation_predictions = np.array([x.strip() == 'True' for x in open(s2s_validation_predictions_path).readlines()])
                         s2s_test_predictions = np.array([x.strip() == 'True' for x in open(s2s_test_predictions_path).readlines()])
                         print('s2s true acc (VAL):', 100 * (np.sum(s2s_validation_predictions)/len(s2s_validation_predictions)))
