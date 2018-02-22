80fd3a180c4a803d5760577d84c5cb2628742a68
Ben Robaidek <brobaidek@gmail.com>
2018-02-21 23:25:08 -0800

    test batch size

--- mawps/solver.py
+++ mawps/solver.py
@@ -10,11 +10,12 @@ def solve(equations, variables, answers):
     corrects = np.array([])
     test_dicts = open('../mawps/data/test.dicts').readlines()
     test = [x.split('\t')[1] for x in open('../mawps/data/test.tsv').readlines()]
+    print(len(equations), len(test), len(test_dicts))
     for pred_eq, gold_eq, var in zip(equations, test, test_dicts):
         var = eval(var)
-        #print('pred_eq', pred_eq)
-        #print('gold_eq', gold_eq)
-        #print('var', var)
+        print('pred_eq', pred_eq)
+        print('gold_eq', gold_eq)
+        print('var', var)
 
         for k in var.keys():
             pred_eq = pred_eq.replace(var.get(k), str(k))
